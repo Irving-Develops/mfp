c3d9d6c509925680fca701bb0b892432cf210329
Irving Arreola Palacios <irving.a789@gmail.com>
2023-08-10 12:57:30 -0500

    testing domain again

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -4,20 +4,20 @@ const commonConfig = require("./webpack.common");
 const packageJson = require("../package.json");
 
 const domain = process.env.PRODUCTION_DOMAIN;
-console.log("domain", domain);
+
 
 const prodConfig = {
   mode: "production",
   output: {
     filename: "[name].[contenthash].js", //done for cashing issues
-    publicPath: "/container/latest/",
+    publicPath: "/container/latest/"
   },
 
   plugins: [
     new ModuleFederationPlugin({
       name: "container",
       remotes: {
-        marketing: `marketing@https://d276h9s8aq7fvu.cloudfront.net/marketing/latest/remoteEntry.js`,
+        marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
       },
       shared: packageJson.dependencies,
     }),
