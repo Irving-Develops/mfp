2e826a1d79ae6995909bc03eae38eae2435a188a
Irving Arreola Palacios <irving.a789@gmail.com>
2023-08-10 13:25:25 -0500

    added header to app

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import {mount} from 'marketing/MarketingApp'
 import {BrowserRouter} from 'react-router-dom'
 import MarketingApp from './components/MarketingApp'
+import Header from './components/Header'
 
 console.log(mount)
 const App = () => {
   return (
     <BrowserRouter>
       <div>
+        <Header />
         <MarketingApp />
       </div>
     </BrowserRouter>
